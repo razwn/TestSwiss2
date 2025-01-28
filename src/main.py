61029b3f6abde60ce2527707af4b12a1eb23ff16
Razvan Ritivoi <razvan.ritivoi@vodafone.com>
2025-01-28 18:06:58 +0200

    Load environment variables from .env file and print PAROLA_DB

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from utils.util import print_hello
 from utils.api_requests import APIS
+# pip install python-dotenv
+from dotenv import load_dotenv
 import os
-
+load_dotenv(".env")
 
 print_hello()
 print(os.environ.get("API_KEY"))
+print(os.environ.get("PAROLA_DB"))
